5b4b7beb6e87fa46515c1cf1920e17589720ed27
kazimad <kazimad.kr@gmail.com>
2014-03-29 23:25:55 +0200

    11

--- src/main/java/com/example/myapplication/app/MainActivity.java
+++ src/main/java/com/example/myapplication/app/MainActivity.java
@@ -1,89 +1,131 @@
 package com.example.myapplication.app;
-
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import org.w3c.dom.Text;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.SocketException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class MainActivity extends ActionBarActivity {
-    static File myFileWithLinks = new File ("links.txt");
-    static String nameDir = "cats/";
-    static File myDir = new File(nameDir);
-    final LinkedBlockingQueue <String> myStringQueue = new LinkedBlockingQueue<String>();
+    private Bitmap myBitmap;
+    static int numberOfThreads = 4;
+    String myLinks;
+    String ImagePath;
 
-    String links;
-    int countLinks = 1;
-    int countNameFiles = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        final ArrayList <DownloadThread> myListOfThread = new ArrayList<DownloadThread>();
+        final LinkedBlockingQueue <String> myStringQueue = new LinkedBlockingQueue<String>();                 // queue of links to download
+        final LinkedBlockingQueue<String> myImages = new LinkedBlockingQueue<String>();                                   //  queue of images
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button download = (Button) findViewById(R.id.download);
-        Text CatDownload = (Text) findViewById(R.id.CatDownload);
+        final Button download = (Button) findViewById(R.id.download);
+//        Text CatDownload = (Text) findViewById(R.id.CatDownload);
+        final ImageView myImageViev = (ImageView) findViewById(R.id.imageView);
+        final DownloadThread myThread = new DownloadThread();
+        download.setEnabled(true);
 
-        makeDir();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean isCDMount = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+                if (isCDMount) {
+                    ImagePath = String.valueOf(getExternalCacheDir());
+                } else {
+                    ImagePath = String.valueOf(getCacheDir());
+                }
 
-
+            }
+        }).start();
 
         download.setOnClickListener(new View.OnClickListener() {
 
+
+
             @Override
             public void onClick(View view) {
-                makeDir();
+                download.setEnabled(false);
 
                 try {
-                    BufferedReader bf = new BufferedReader(new FileReader(myFileWithLinks));
-                    while ((links = bf.readLine()) != null) {
-                        myStringQueue.add(links);
+                    BufferedReader bf = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.links)));     // читает фаил со ссылками из папки raw
+                    while ((myLinks = bf.readLine()) != null) {
+
+                        myStringQueue.add(myLinks);
+                    }
+                    for (int i = 0; i <numberOfThreads; i++) {
+                        myListOfThread.add(myThread);
+
+                        myThread.start();
+                    }
+                    while (!myStringQueue.isEmpty()) {
+                        myThread.setDownloadMyLink(myStringQueue.take());
+                    }
+                    for (DownloadThread myThread : myListOfThread) {
+                        myThread.join();
                     }
 
 
-                    new Thread(new Runnable() {
 
-                        String myLInk = myStringQueue.take();
+                    myThread.setMyPathDownload(ImagePath);
+
+                    new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            if (!myStringQueue.isEmpty()) {                 // тут надо было бы реализовать CallBack - но пока не понятно как...
+                            myImages = myThread.getMyImages();
+                            while (!myImages.isEmpty()) {
 
-
-                                String outFile = ("cats/newfile_" + countNameFiles + ".jpg");
+                                String displayedImage = null;
                                 try {
-                                    saveImage(myLInk, outFile, countLinks);
-                                } catch (IOException e) {
+                                    displayedImage = myImages.take();
+                                } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
-                                countNameFiles++;
-                                countLinks++;
+
+                                BitmapFactory.Options myOptions  = new BitmapFactory.Options();
+                                myOptions.inJustDecodeBounds = true;
+                                int memory  = myOptions.outHeight*myOptions.outWidth*4;
+                                myOptions.inJustDecodeBounds=false;
+                                long allMemory = Runtime.getRuntime().freeMemory();
+                                if (memory>allMemory){
+                                    myOptions.inSampleSize = 4;
+                                }
+                                myBitmap = BitmapFactory.decodeFile(displayedImage,myOptions);
+                                myImageViev.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        myImageViev.setImageBitmap(myBitmap);
+
+                                    }
+                                });
                             }
                         }
                     }).start();
-
-//                File [] imageArray = myDir.listFiles();
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//
-//
-//                    }
-//                }).start();
-
 
 
 
@@ -95,21 +137,16 @@ public class MainActivity extends ActionBarActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
             }
 
         });
-
-
-
     }
 
     protected static void saveImage(String link, String outFile, int countLinks) throws IOException {
         try {
             URL url = new URL(link);
-            InputStream is = url.openStream();
+            InputStream is = new BufferedInputStream(url.openStream());
             OutputStream os = new FileOutputStream(outFile);
-
             int length;
             while ((length = is.read()) != -1) { // reads by bytes while it is possible
                 os.write(length);
@@ -125,13 +162,6 @@ public class MainActivity extends ActionBarActivity {
             System.out.println("link #" + countLinks + " is unavailable3");
         }
     }
-
-    public static void makeDir () {
-        if (!myDir.exists()) {
-            myDir.mkdir();
-        }
-    }
-
+////////
+//
 }
-////////////
-///
