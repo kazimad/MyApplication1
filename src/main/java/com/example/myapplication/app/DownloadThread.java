380017e71acf06239bf503ced4d0d91f2154b47c
kazimad <kazimad.kr@gmail.com>
2014-03-29 23:23:03 +0200

    11

---
+++ src/main/java/com/example/myapplication/app/DownloadThread.java
@@ -0,0 +1,53 @@
+package com.example.myapplication.app;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+public class DownloadThread extends Thread {
+
+
+    static final Object myMutex = new Object();
+    int countLinks = 1;
+    int countNameFiles = 1;
+    public String downloadMyLink;
+    public String myPathDownload;
+
+
+
+    public LinkedBlockingQueue<String> myImages = new LinkedBlockingQueue();
+
+    public void setDownloadMyLink(String downloadMyLink) {
+        this.downloadMyLink = downloadMyLink;
+    }
+//    MainActivity mainActivity = new MainActivity();
+    public void setMyPathDownload(String myPathDownload) {
+        this.myPathDownload = myPathDownload;
+    }
+    public LinkedBlockingQueue<String> getMyImages() {
+        return myImages;
+    }
+
+
+    @Override
+    public void run() {
+        synchronized (myMutex){
+            countLinks++;
+            countNameFiles++;
+        }
+        String outFile = ( myPathDownload + "/newfile_" + countNameFiles + ".jpg");
+        try {
+            MainActivity.saveImage(downloadMyLink, outFile, countLinks);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        mainActivity.myImagesAdd(outFile);
+//            mainActivity.setOutFile(outFile);
+        myImages.add(outFile);
+    }
+}
+
